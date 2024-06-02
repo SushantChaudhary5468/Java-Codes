3cca6844cead8066a7eb52441a9c3e433fcc8c16
SushantChaudhary5468 <126761795+SushantChaudhary5468@users.noreply.github.com>
2024-06-02 19:44:10 +0530

    Add files via upload

---
+++ Lec12.java
@@ -0,0 +1,18 @@
+public class Lec12 {
+    public static void hollowRectangle(int rows, int columns) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        hollowRectangle(6, 4);
+    }
+}
